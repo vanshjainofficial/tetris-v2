a06e792703e7f90db49ccad7867425d01b868087
Vansh Jain <vj8067202@gmail.com>
2025-06-12 05:59:39 +0530

    Fix: Lint errors in component and main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { enableProdMode, ErrorHandler, importProvidersFrom } from '@angular/core
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import * as Sentry from '@sentry/angular';
-import { Integrations } from '@sentry/tracing';
 import { AppComponent } from '@angular-tetris/app.component';
 import { environment } from './environments/environment';
 
